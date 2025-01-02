694ec027af1813a09d990ecd5415b18392abe360
phuonglinhle268 <plinhle26@gmail.com>
2025-01-02 13:25:07 +0700

    Add files via upload

---
+++ function.h
@@ -0,0 +1,11 @@
+#include "datatype.h"
+//Nguyen mau ham 
+struct Student students[50];
+int totalStudents;
+void printMenu();
+void printMenustudent();
+void addStudent(struct Student *std);
+void showAll(struct Student students[50], int n);
+void editInformation();
+void back_or_exit();
+void sort_Student_i_or_d();
